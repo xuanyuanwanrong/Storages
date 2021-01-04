16d1a6f2853ec7b192cc9d28a617af052ecb28e0
天罡 袁 <3313990072@qq.com>
2021-01-04 15:28:19 +0800

    分页类

---
+++ Models/PageList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+   public class PageList
+    {
+        //数据集合
+        public IQueryable DataList { get; set; }
+
+        //总行数
+        public int PageCount { get; set; }
+    }
+}
--- Storages/Controllers/Ma/MaController.cs
+++ Storages/Controllers/Ma/MaController.cs
@@ -8,14 +8,7 @@ namespace Storage.Controllers.Ma
 {
     public class MaController : Controller
     {
-        // GET: Ma
-<<<<<<< HEAD
-        /// <summary>
-        /// 跳转主页
-        /// </summary>
-        /// <returns></returns>
-=======
->>>>>>> 6e20f79feae093604c43eae9d44cf58e408c7197
+        //跳转主页
         public ActionResult Index()
         {
             return Redirect("Ma/index.html");
