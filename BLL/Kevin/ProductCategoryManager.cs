8ec318eb6d52e5e465e6edf67686e28ddfd0b1d8
Kevin-Liu-Father <1742855207@qq.com>
2021-01-05 10:12:47 +0800

    99999

---
+++ BLL/Kevin/ProductCategoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL.Kevin;
+
+namespace BLL.Kevin
+{
+    public class ProductCategoryManager
+    {
+        public static PageList ProductCategoryList(int PageIndex, int PageSize)
+        {
+            return ProductCategoryService.ProductCategoryList(PageIndex, PageSize);
+        }
+    }
+}
