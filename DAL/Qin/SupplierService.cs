5b018ae1a3b3f0d07f9abc49617ed4a6da98b2d3
zq03015 <1756843290@qq.com>
2021-01-05 11:17:14 +0800

    kiiimi

---
+++ DAL/Qin/SupplierService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAL.Qin
+{
+    public class SupplierService
+    {
+        public static IQueryable GetAll()
+        {
+            StorageEntities entity = new StorageEntities();
+            var obj = from p in entity.Supplier
+                      select new
+                      {
+                          SlId = p.SlId,
+                          SupplierTypeName = p.SupplierType.SupplierTypeName,
+                          SlrName = p.SlrName,
+                          SlDescribe = p.SlDescribe,
+                          SlContacts = p.SlContacts,
+                          SlIphone = p.SlIphone,
+                          SlAddress = p.SlAddress
+                      };
+            return obj;
+        }
+    }
+}
