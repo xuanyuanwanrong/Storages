0320fdf17963f284538cb1679aad705cbb4c12db
hello <1744840108@qq.com>
2021-01-05 16:48:10 +0800

    lyx

---
+++ BLL/LiuYingXuan/WareHouseManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DAL.LiuYingXuan;
+
+namespace BLL.LiuYingXuan
+{
+   public  class WareHouseManager
+    {
+        public static IQueryable GetWareHouse()
+        {
+            return WareHouseService.GetWareHouse();
+        }
+    }
+}
