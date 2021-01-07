f653080b21221a84e02b976d10c3b7c2eddb56dc
lxy0707 <2825960167@qq.com>
2021-01-07 17:23:16 +0800

    lxy

--- BLL/Lxy/RuKumanager.cs
+++ BLL/Lxy/RuKumanager.cs
@@ -27,11 +27,7 @@ namespace BLL.Lxy
         #endregion
 
         #region 入库单查询待审核
-        //获取入库单总条数
-        public static int RuKuDSHRows()
-        {
-            return RuKu.RuKuDSHRows();
-        }
+       
 
         //查询待审核
         public static PageList SelectRuKuDSH(int pageIndex, int PageSize)
@@ -40,12 +36,7 @@ namespace BLL.Lxy
         }
         #endregion
         #region 入库单查询审核成功
-        //获取入库单总条数
-        public static int RuKuSHCGRows()
-        {
-            return RuKu.RuKuSHCGRows();
-        }
-
+       
         //查询待审核
         public static PageList SelectRuKuSHCG(int pageIndex, int PageSize)
         {
@@ -53,12 +44,7 @@ namespace BLL.Lxy
         }
         #endregion
         #region 入库单查询审核失败
-        //获取入库单总条数
-        public static int RuKuSHSBRows()
-        {
-            return RuKu.RuKuSHSBRows();
-        }
-
+       
         //查询待审核
         public static PageList SelectRuKuSHSB(int pageIndex, int PageSize)
         {
@@ -70,10 +56,79 @@ namespace BLL.Lxy
 
         #region 根据入库单号查询
         
-
+        //所有
         public static PageList SelectRuKuID(int pageIndex, int PageSize, int id)
         {
             return RuKu.SelectRuKuID(pageIndex,PageSize,id);
+        }
+
+        //查询待审核
+        public static PageList SelectRuKuDSHId(int pageIndex, int PageSize, int id)
+        {
+            return RuKu.SelectRuKuDSHId(pageIndex, PageSize, id);
+        }
+
+        //查询审核成功
+        public static PageList SelectRuKuSHCGId(int pageIndex, int PageSize, int id)
+        {
+            return RuKu.SelectRuKuSHCGId(pageIndex, PageSize, id);
+        }
+
+        //查询审核失败
+        public static PageList SelectRuKuSHSBId(int pageIndex, int PageSize, int id)
+        {
+            return RuKu.SelectRuKuSHSBId(pageIndex, PageSize, id);
+        }
+        #endregion
+
+        #region 根据入库时间查询
+        //所有
+        public static PageList SelectRuKuTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            return RuKu.SelectRuKuTime(pageIndex, PageSize, timeL,timeR);
+        }
+
+        //查询待审核
+        public static PageList SelectRuKuDSHTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            return RuKu.SelectRuKuDSHTime(pageIndex, PageSize, timeL,timeR);
+        }
+
+        //查询审核成功
+        public static PageList SelectRuKuSHCGTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            return RuKu.SelectRuKuSHCGTime(pageIndex, PageSize, timeL,timeR);
+        }
+
+        //查询审核失败
+        public static PageList SelectRuKuSHSBTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            return RuKu.SelectRuKuSHSBTime(pageIndex, PageSize,timeL,timeR );
+        }
+        #endregion
+
+        #region 两者查询
+        public static PageList SelectRuKuIDTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR, int id)
+        {
+            return RuKu.SelectRuKuIDTime(pageIndex,PageSize,timeL,timeR,id);
+        }
+
+        //查询待审核
+        public static PageList SelectRuKuDSHTimeId(int pageIndex, int PageSize, DateTime timeL, DateTime timeR, int id)
+        {
+            return RuKu.SelectRuKuDSHTimeId(pageIndex, PageSize, timeL, timeR,id);
+        }
+
+        //查询审核成功
+        public static PageList SelectRuKuSHCGTimeId(int pageIndex, int PageSize, DateTime timeL, DateTime timeR, int id)
+        {
+            return RuKu.SelectRuKuSHCGTimeId(pageIndex, PageSize, timeL, timeR,id);
+        }
+
+        //查询审核失败
+        public static PageList SelectRuKuSHSBTimeId(int pageIndex, int PageSize, DateTime timeL, DateTime timeR, int id)
+        {
+            return RuKu.SelectRuKuSHSBTimeId(pageIndex, PageSize, timeL, timeR,id);
         }
         #endregion
         #endregion
--- DAL/Lxy/RuKu.cs
+++ DAL/Lxy/RuKu.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Models;
+using System.Data;
 namespace DAL.Lxy
 {
    public class RuKu
@@ -16,7 +17,8 @@ namespace DAL.Lxy
         public static int RuKuRows()
         {
             StorageEntities test = new StorageEntities();
-            return test.Warehousing.Count();
+            var obj = from p in test.Warehousing where p.WState != 0 select p;
+            return obj.Count();
         }
 
         public static PageList SelectRuKuAll(int pageIndex, int PageSize)
@@ -24,7 +26,7 @@ namespace DAL.Lxy
             StorageEntities test = new StorageEntities();
             //实例化分页类
             PageList list = new PageList();
-            var obj = from p in test.Warehousing
+            var obj = from p in test.Warehousing where p.WState!=0
                       orderby p.Wid
                       select new
                       {
@@ -48,13 +50,8 @@ namespace DAL.Lxy
         #endregion
 
         #region 入库单查询待审核
-        //获取入库单总条数
-        public static int RuKuDSHRows()
-        {
-            StorageEntities test = new StorageEntities();
-            var obj = from p in test.Warehousing where p.Slid ==1 select p;
-            return obj.Count();
-        }
+        
+       
 
         //查询待审核
         public static PageList SelectRuKuDSH(int pageIndex, int PageSize)
@@ -63,7 +60,7 @@ namespace DAL.Lxy
             //实例化分页类
             PageList list = new PageList();
             var obj = from p in test.Warehousing
-                      where p.Slid ==1
+                      where p.WState ==1
                       orderby p.Wid
                       select new
                       {
@@ -78,30 +75,23 @@ namespace DAL.Lxy
                           time = p.time
                       };
             list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
-            int rows = test.Warehousing.Count();
+            
             //设置总页数
-            list.PageCount = rows % PageSize == 0 ? rows / PageSize : rows / PageSize + 1;
+            list.PageCount = obj.Count();
             return list;
         }
         #endregion
 
         #region 入库单查询审核成功
-        //获取入库单总条数
-        public static int RuKuSHCGRows()
-        {
-            StorageEntities test = new StorageEntities();
-            var obj = from p in test.Warehousing where p.Slid == 2 select p;
-            return obj.Count();
-        }
-
-        //查询待审核
+       
+        //查询审核成功
         public static PageList SelectRuKuSHCG(int pageIndex, int PageSize)
         {
             StorageEntities test = new StorageEntities();
             //实例化分页类
             PageList list = new PageList();
             var obj = from p in test.Warehousing
-                      where p.Slid == 2
+                      where p.WState == 2
                       orderby p.Wid
                       select new
                       {
@@ -116,30 +106,24 @@ namespace DAL.Lxy
                           time = p.time
                       };
             list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
-            int rows = test.Warehousing.Count();
+
             //设置总页数
-            list.PageCount = rows % PageSize == 0 ? rows / PageSize : rows / PageSize + 1;
+            list.PageCount = obj.Count();
             return list;
         }
         #endregion
 
-        #region 入库单查询审核成功
-        //获取入库单总条数
-        public static int RuKuSHSBRows()
-        {
-            StorageEntities test = new StorageEntities();
-            var obj = from p in test.Warehousing where p.Slid == 3 select p;
-            return obj.Count();
-        }
+        #region 入库单查询审核失败
+       
 
-        //查询待审核
+        //查询审核失败
         public static PageList SelectRuKuSHSB(int pageIndex, int PageSize)
         {
             StorageEntities test = new StorageEntities();
             //实例化分页类
             PageList list = new PageList();
             var obj = from p in test.Warehousing
-                      where p.Slid == 3
+                      where p.WState == 3
                       orderby p.Wid
                       select new
                       {
@@ -154,9 +138,8 @@ namespace DAL.Lxy
                           time = p.time
                       };
             list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
-            int rows = test.Warehousing.Count();
-            //设置总页数
-            list.PageCount = rows % PageSize == 0 ? rows / PageSize : rows / PageSize + 1;
+            
+            list.PageCount = obj.Count();
             return list;
         }
         #endregion
@@ -165,6 +148,7 @@ namespace DAL.Lxy
 
         #region 根据入库单号查询
         
+        //所有
         public static PageList SelectRuKuID(int pageIndex, int PageSize,int id)
         {
             StorageEntities test = new StorageEntities();
@@ -186,12 +170,319 @@ namespace DAL.Lxy
                           time = p.time
                       };
             list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
-            int rows = test.Warehousing.Count();
+          
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询待审核
+        public static PageList SelectRuKuDSHId(int pageIndex, int PageSize,int id)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 1&&p.Wid==id
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
             //设置总页数
-            list.PageCount = rows % PageSize == 0 ? rows / PageSize : rows / PageSize + 1;
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询审核成功
+        public static PageList SelectRuKuSHCGId(int pageIndex, int PageSize,int id)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 2 && p.Wid==id
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            //设置总页数
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询审核失败
+        public static PageList SelectRuKuSHSBId(int pageIndex, int PageSize,int id)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 3&&p.Wid==id
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            list.PageCount = obj.Count();
+            return list;
+        }
+        #endregion
+
+        #region 根据日期查询
+        //所有
+        public static PageList SelectRuKuTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.time>=timeL && p.time<=timeR
+                      orderby p.Wid 
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          stateid = p.Slid,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            list.PageCount = obj.Count();
+            return list;
+        }
+        //查询待审核
+        public static PageList SelectRuKuDSHTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 1 && p.time >= timeL && p.time <= timeR
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            //设置总页数
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询审核成功
+        public static PageList SelectRuKuSHCGTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 2 && p.time >= timeL && p.time <= timeR
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            //设置总页数
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询审核失败
+        public static PageList SelectRuKuSHSBTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 3 && p.time >= timeL && p.time <= timeR
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            list.PageCount = obj.Count();
+            return list;
+        }
+        #endregion
+        #region 两者查询
+        public static PageList SelectRuKuIDTime(int pageIndex, int PageSize, DateTime timeL, DateTime timeR,int id)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.time >= timeL && p.time <= timeR && p.Wid==id
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          stateid = p.Slid,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询待审核
+        public static PageList SelectRuKuDSHTimeId(int pageIndex, int PageSize, DateTime timeL, DateTime timeR,int id)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 1 && p.time >= timeL && p.time <= timeR&&p.Wid==id
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            //设置总页数
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询审核成功
+        public static PageList SelectRuKuSHCGTimeId(int pageIndex, int PageSize, DateTime timeL, DateTime timeR,int id)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 2 && p.time >= timeL && p.time <= timeR&&p.Wid==id
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            //设置总页数
+            list.PageCount = obj.Count();
+            return list;
+        }
+
+        //查询审核失败
+        public static PageList SelectRuKuSHSBTimeId(int pageIndex, int PageSize, DateTime timeL, DateTime timeR,int id)
+        {
+            StorageEntities test = new StorageEntities();
+            //实例化分页类
+            PageList list = new PageList();
+            var obj = from p in test.Warehousing
+                      where p.WState == 3 && p.time >= timeL && p.time <= timeR&&p.Wid==id
+                      orderby p.Wid
+                      select new
+                      {
+                          wid = p.Wid,
+                          tname = p.WareStateType.TName,
+                          sname = p.Supplier.SlrName,
+                          rid = p.Rid,
+                          num = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPnum,
+                          price = from pp in p.WareProduct where p.Wid == pp.Wid select pp.WPprice,
+                          state = p.WareState.Sname,
+                          user = p.User.UserName,
+                          time = p.time
+                      };
+            list.DataList = obj.Skip((pageIndex - 1) * PageSize).Take(PageSize);
+
+            list.PageCount = obj.Count();
             return list;
         }
         #endregion
         #endregion
+
+
     }
 }
