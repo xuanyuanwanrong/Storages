4db7e244f3c1370425309d758f88e677ebf13e28
Kevin-Liu-Father <1742855207@qq.com>
2021-01-07 21:36:28 +0800

    lkw

---
+++ DAL/Kevin/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace DAL.Kevin
+{
+    public class CustomerService
+    {
+        public static IQueryable GetCustomer()
+        {
+            StorageEntities entities = new StorageEntities();
+            var obj = from p in entities.Customer
+                      select new
+                      {
+                          CtId = p.CtId,
+                          CtName = p.CtName
+                      };
+            return obj;
+        }
+    }
+}
